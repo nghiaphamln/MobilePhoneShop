5966fdbc709e07f2f2e472ea3c55a7a0d1ee452e
pspking2704 <67267067+pspking2704@users.noreply.github.com>
2021-11-28 14:51:22 +0700

    update

--- MobilePhoneShop/MobilePhoneShop/Controllers/DangKyController.cs
+++ MobilePhoneShop/MobilePhoneShop/Controllers/DangKyController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MobilePhoneShop.Models;
 
 namespace MobilePhoneShop.Controllers
 {
     public class DangKyController : Controller
     {
+        UserProfile user;
+        public bool active = true;
+
         // GET: Register
         public ActionResult Index()
         {
+            if(Session["userName"] != null)
+            {
+                return RedirectToAction("Index","TrangChu");
+            }
+          
             return View();
+
         }
     }
 }
--- MobilePhoneShop/MobilePhoneShop/Controllers/DangNhapController.cs
+++ MobilePhoneShop/MobilePhoneShop/Controllers/DangNhapController.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MobilePhoneShop.Models;
+using System;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MobilePhoneShop.Controllers
 {
     public class DangNhapController : Controller
     {
+
         // GET: DangNhap
         public ActionResult Index()
         {
+            if(Session["userName"] != null)
+            {
+                return RedirectToAction("Index", "TrangChu");
+            }
+
             return View();
         }
 
@@ -18,5 +23,34 @@ namespace MobilePhoneShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Authorise(UserProfile user)
+        {
+            using (MobilePhoneShopEntities db = new MobilePhoneShopEntities())
+            {
+                var userDetail = db.UserProfiles.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+                if (userDetail == null)
+                {
+                    user.LoginErrorMsg = "Tài khoản hoặc mật khẩu không tồn tại";
+                    return View("Index", user);
+
+                }
+                else
+                {
+                    /*                    user.ActiveFlat = true;*/
+                    Session["userName"] = user.UserName;
+                    return RedirectToAction("Index", "TrangChu");
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "TrangChu");
+        }
+
+
     }
 }
